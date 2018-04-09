70126e192bd176d595a764be9d8523d0715b401e
gulci-poz <sebastian.gulczynski.poznan@gmail.com>
2018-04-09 18:56:49 +0200

    testing arithmetic operations

--- src/main/java/sda/mvn/calculator/Calculator.java
+++ src/main/java/sda/mvn/calculator/Calculator.java
@@ -5,4 +5,16 @@ public class Calculator {
     public int sum(int a, int b) {
         return a + b;
     }
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
+
+    public int mult(int a, int b) {
+        return a * b;
+    }
+
+    public int div(int a, int b) {
+        return a / b;
+    }
 }
--- src/test/java/sda/mvn/calculator/CalculatorTest.java
+++ src/test/java/sda/mvn/calculator/CalculatorTest.java
@@ -1,14 +1,35 @@
 package sda.mvn.calculator;
 
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class CalculatorTest {
 
+    private static Calculator calculator;
+
+    @BeforeClass
+    public static void prepareTests() {
+        calculator = new Calculator();
+    }
+
     @Test
     public void testSum() {
-        Calculator calculator = new Calculator();
-        int sum = calculator.sum(3, 5);
-        Assert.assertEquals("result is not 8", sum, 8);
+        Assert.assertEquals("result is not 8", calculator.sum(3, 5), 8);
+    }
+
+    @Test
+    public void testsub() {
+        Assert.assertEquals("result is not -2", calculator.sub(3, 5), -2);
+    }
+
+    @Test
+    public void testMult() {
+        Assert.assertEquals("result is not 15", calculator.mult(3, 5), 15);
+    }
+
+    @Test
+    public void testDiv() {
+        Assert.assertEquals("result is not 5", calculator.div(11, 2), 5);
     }
 }
