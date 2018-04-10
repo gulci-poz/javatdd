fabe49505e1bd0db8f95a79ae7facf3666fecb72
gulci-poz <sebastian.gulczynski.poznan@gmail.com>
2018-04-10 18:42:07 +0200

    StringCalculatedTest corrected

--- src/test/java/sda/mvn/utils/StringCalculatorTest.java
+++ src/test/java/sda/mvn/utils/StringCalculatorTest.java
@@ -15,25 +15,28 @@ public class StringCalculatorTest {
 
     @Test
     public void sumStringTest() {
-        Assert.assertEquals("not adding properly", stringCalculator.sumString("99,  3"), 102);
-        Assert.assertEquals("not adding properly", stringCalculator.sumString("4, 17"), 21);
-        Assert.assertEquals("not adding properly", stringCalculator.sumString("21,7"), 28);
+        Assert.assertEquals("not adding properly", 102, stringCalculator.sumString("99,  3"));
+        Assert.assertEquals("not adding properly", 21, stringCalculator.sumString("4, 17"));
+        Assert.assertEquals("not adding properly", 28, stringCalculator.sumString("21,7"));
         Assert.assertEquals("not adding properly",
-                stringCalculator.sumString("21,7, 43,    34,  323"),
-                428);
+                428,
+                stringCalculator.sumString("21,7, 43,    34,  323")
+        );
     }
 
     @Test
     public void sumStringNull() {
         Assert.assertEquals("not adding properly for blank or null",
-                stringCalculator.sumString(null),
-                0);
+                0,
+                stringCalculator.sumString(null)
+        );
     }
 
     @Test
     public void sumStringBlank() {
         Assert.assertEquals("not adding properly for blank or null",
-                stringCalculator.sumString(""),
-                0);
+                0,
+                stringCalculator.sumString("")
+        );
     }
 }
